e74df0020cc721efc0286f8be6f5f148d891ca9b
pallugowda <kavyapallavi424@gmail.com>
2024-08-01 11:45:06 +0530

    a12

--- ai1.py
+++ ai1.py
@@ -1,6 +1,1 @@
-import pandas as p
-d=p.DataFrame([[2,4,5],[9,8,3],[11,2,1]],columns=["maths","java","python"])
-print(d)
-c=d.agg(['sum','min','max','count','mean','std','size'])
-print()
-print(c)
+print("helo")
